551b03ac7ffbef9dca3ab4f4109d96347b4ab3e6
Shyamk070 <skebboosted2004@gmail.com>
2025-04-19 15:03:50 +0530

    Update wait-for-psql.py

--- 18.0/wait-for-psql.py
+++ 18.0/wait-for-psql.py
@@ -1,32 +1,20 @@
-#!/usr/bin/env python3
-import argparse
-import psycopg2
-import sys
+#!/usr/bin/env python
+import os
 import time
+import psycopg2
 
+while True:
+    try:
+        conn = psycopg2.connect(
+            dbname=os.environ.get('POSTGRES_DB', 'postgres'),
+            user=os.environ.get('POSTGRES_USER', 'odoo'),
+            password=os.environ.get('POSTGRES_PASSWORD', 'odoo'),
+            host=os.environ.get('DB_HOST', 'db'),
+            port=os.environ.get('DB_PORT', '5432'),
+        )
+        conn.close()
+        break
+    except Exception as e:
+        print("Waiting for PostgreSQL...")
+        time.sleep(2)
 
-if __name__ == '__main__':
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('--db_host', required=True)
-    arg_parser.add_argument('--db_port', required=True)
-    arg_parser.add_argument('--db_user', required=True)
-    arg_parser.add_argument('--db_password', required=True)
-    arg_parser.add_argument('--timeout', type=int, default=5)
-
-    args = arg_parser.parse_args()
-
-    start_time = time.time()
-    while (time.time() - start_time) < args.timeout:
-        try:
-            conn = psycopg2.connect(user=args.db_user, host=args.db_host, port=args.db_port, password=args.db_password, dbname='postgres')
-            error = ''
-            break
-        except psycopg2.OperationalError as e:
-            error = e
-        else:
-            conn.close()
-        time.sleep(1)
-
-    if error:
-        print("Database connection failure: %s" % error, file=sys.stderr)
-        sys.exit(1)
